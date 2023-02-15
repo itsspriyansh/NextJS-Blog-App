a5ee81a7d4104770f1197dc51c658123ed11ce0c
itsspriyansh <priyanshkp01@gmail.com>
2023-02-15 21:24:01 +0530

    added layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '@/styles/globals.scss'
 import 'tailwindcss/tailwind.css'
+import { Layout } from '../components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
