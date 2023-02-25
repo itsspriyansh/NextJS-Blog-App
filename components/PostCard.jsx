36c8bd70715ce1d0949a0619b0dd2b25873ec20b
itsspriyansh <priyanshkp01@gmail.com>
2023-02-25 22:41:45 +0530

    added slug

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,16 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 const PostCard = ({ post }) => {
   console.log(post)
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+
         <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
           <img
             src = {post.featuredImage.url}
             alt = {post.title}
-            className = " object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+            className = " top-0 absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
           />
         </div>
+
+        <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+          <Link href={`post/${post.slug}`}>
+            {post.title}
+          </Link>
+        </h1>        
     </div>
   )
 }
