546a030ac28284f641f8425115dca1a53f1451ef
itsspriyansh <priyanshkp01@gmail.com>
2023-02-26 11:54:57 +0530

    added post widget

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 const PostCard = ({ post }) => {
-  console.log(post)
+
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
 
