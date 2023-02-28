c5c6bcf64191ad35772539aa1f53ba2e6d397202
itsspriyansh <priyanshkp01@gmail.com>
2023-02-28 11:54:29 +0530

    added handlers for comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -3,12 +3,37 @@ import React, { useRef, useState } from 'react'
 const CommentsForm = ({ slug }) => {
 
   const [error, setError] = useState(false)
-  const [localStorage, setLocalStorage] = useState(null)
+  // const [localStorage, setLocalStorage] = useState(null)
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
   const commentEl = useRef()
   const nameEl = useRef()
   const emailEl = useRef()
   const storeDataEl = useRef()
+
+  const handleCommentSubmission = () => {
+
+    setError(prev => false)
+
+    const comment = commentEl.current.value
+    const name = nameEl.current.value
+    const email = emailEl.current.value
+    const storeData = storeDataEl.current.checked
+
+    if (!comment || !name || !email) {
+      setError(prev => true)
+      return
+    }
+
+    const commentObj = {comment, name, email, slug}
+
+    if (storeData) {
+      localStorage.setItem("name", name)
+      localStorage.setItem("email", email)
+    } else {
+      localStorage.removeItem("name", name)
+      localStorage.removeItem("email", email)
+    }
+  }
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
@@ -16,22 +41,20 @@ const CommentsForm = ({ slug }) => {
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea 
           ref={commentEl}
-          className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
+          className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700 resize-none" 
           name="comment" 
           placeholder="Comment" />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
         <input 
           type="text" 
-          value={formData.name} 
-          onChange={onInputChange} 
+          ref={nameEl}
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
           placeholder="Name" 
           name="name" />
         <input 
           type="email" 
-          value={formData.email} 
-          onChange={onInputChange} 
+          ref={emailEl} 
           className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
           placeholder="Email" 
           name="email" />
@@ -39,20 +62,24 @@ const CommentsForm = ({ slug }) => {
       <div className="grid grid-cols-1 gap-4 mb-4">
         <div>
           <input 
-            checked={formData.storeData} 
-            onChange={onInputChange} 
+            ref={storeDataEl}
             type="checkbox" 
             id="storeData" 
             name="storeData" 
-            value="true" />
-          <label className="text-gray-500 cursor-pointer" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
+            value="true" 
+          />
+          <label 
+            className="text-gray-500 cursor-pointer ml-2" 
+            htmlFor="storeData"> 
+            Save my name, email in this browser for the next time I comment.
+          </label>
         </div>
       </div>
       {error && <p className="text-xs text-red-500">All fields are mandatory</p>}
       <div className="mt-8">
         <button 
           type="button" 
-          onClick={handlePostSubmission} 
+          onClick={handleCommentSubmission} 
           className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
             Post Comment
         </button>
